bf753f4954f7192122f322d6ae8b42fc48bd42d0
Michael Palaiologos <Palaiologos1990@gmail.com>
2016-03-15 20:50:18 -0700

    Add header nav links, Post actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
 
   def index
     @posts = Post.all
@@ -14,6 +14,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash.now[:success] = "Post Updated"
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -26,11 +37,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = "Post Deleted"
+    redirect_to root_url
+  end
+
 
   private
 
     def post_params
       params.require(:post).permit(:title, :content)
     end
+
 
 end
